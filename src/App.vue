33bf3c18d5edd05f210a3569ebaaadbe01936378
wangwei <jike_wangwei@163.com>
2019-04-04 17:56:46 +0800

    初始化identify

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <identify :identifyCode.sync="identifyCode" ref="identify" @handleClickPanel="handleClickPanel"></identify>
+    <p>{{identifyCode}}</p>
+  </div>
+</template>
+
+<script>
+import identify from './components/identify'
+export default {
+  name: 'app',
+  components: {identify},
+  data () {
+    return {
+      identifyCode: ''
+    }
+  },
+  methods: {
+    handleClickPanel () {
+      // 刷新验证码
+      this.$refs.identify.refreshCode()
+    }
+  },
+  mounted () {
+    this.handleClickPanel()
+  }
+}
+</script>
